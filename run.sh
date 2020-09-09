7c2c1b8e06da80b54789ce34442589309f314646
qistout <2817253603@qq.com>
2020-09-09 11:05:14 +0800

    添加目前只针对cpp的运行脚本

---
+++ run.sh
@@ -0,0 +1,14 @@
+# 使用方法：./run.sh [题号]
+run() {
+    if ! test -e ./$1.cpp
+    then
+        # cpp文件不存在时复制模板代码，创建对应题号代码并进入vim编辑
+        cp template.cpp $1.cpp
+        vim $1.cpp
+    else
+        # cpp文件存在时，生成运行文件到./exec下并运行 
+        g++ $1.cpp -Wall -O0 -g -o ./exec/$1 && ./exec/$1
+    fi
+}
+
+run $1
