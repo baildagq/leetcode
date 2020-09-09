b0ad6451e30b856ef22469ee4df8394274e2281f
qistout <2817253603@qq.com>
2020-09-09 10:57:12 +0800

    2020/09/08

--- template.cpp
+++ template.cpp
@@ -2,10 +2,12 @@
 
 
 
-int main(int argc, const char** argv) {
-    vector<int> list = {1,2,3,4};
-    // printf("%d\n", Solution().xxx());
-    // printf("%s\n", Solution().xxx(list).c_str());
-    // printVector(Solution().xxx());
+
+
+int main(int argc, const char** argv)
+{
+    vector<int> list = { 1, 2, 3, 4, 5 };
+    vector<vector<int>> lists = { { -240, -657 }, { -27, -188 }, { -616, -247 } };
+    printf("%d\n", Solution().maxPoints(lists));
     return 0;
 }
